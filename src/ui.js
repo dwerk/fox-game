640a3d70d38b1eba890884a1850a29b32710fcba
dwerk <d.werkzam@xpay.de>
2021-04-13 09:49:07 +0200

    test commit

--- src/ui.js
+++ src/ui.js
@@ -1,11 +1,11 @@
 export const modFox = function modFox(state) {
   //when passing the classname will give the appopriate state of the fox
-  document.querySelector(".fox").className = `fox fox-${state}`;
+  document.querySelector(".fox").className = `fox fox--state-${state}`;
 }
 
 export const modScene = function modScene(state) {
   //when passing the classname will give the appopriate scene of the background
-  document.querySelector(".game").className = `game ${state}`;
+  document.querySelector(".game").className = `game game--state-${state}`;
 }
 
 export const togglePoopBag = function togglePoopBag(show) {
